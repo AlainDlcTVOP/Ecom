55e2dd9a8daf61431894bf49d94ac67d5986efac
AlainDlcTVOP <alain_dlc@outlook.com>
2021-07-06 21:03:28 +0200

    first api call done

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,25 @@ require('dotenv/config');
 
 const api = process.env.API_URL;
 
-app.get(api+'/products', (req, res) => {
-    res.send('IDA API');
+//Middleware
+app.use(express.json());
+
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some_url',
+    }
+    res.send(product);
 })
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
+})
+
 app.listen(3000, () => {
-    console.log(api);
+  
     console.log('server is running http://localhost:3000');
 })
