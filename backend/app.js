d3291ffca31a9a0df002fc58778ce3b7267aafe0
AlainDlcTVOP <alain_dlc@outlook.com>
2021-07-06 16:48:49 +0200

    v1

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+app.get(api+'/products', (req, res) => {
     res.send('IDA API');
 })
 app.listen(3000, () => {
+    console.log(api);
     console.log('server is running http://localhost:3000');
 })
