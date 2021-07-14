b10a15e00f9dd2e4718dd8e30139f6bfe62a2463
AlainDlcTVOP <alain_dlc@outlook.com>
2021-07-14 11:24:41 +0200

    Post and Get product Rest API

--- backend/models/product.js
+++ backend/models/product.js
@@ -3,41 +3,41 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     name: {
         type: String,
-        require: true,
+        required: true,
     },
     description: {
         type: String,
-        require: true,
+        required: true
     },
     richDescription: {
         type: String,
-        default: '',
+        default: ''
     },
-    image: [{
+    image: {
         type: String,
-        default: '',
-    }],
-    images: {
-      type: String,  
+        default: ''
     },
+    images: [{
+        type: String
+    }],
     brand: {
         type: String,
-        default: '',
+        default: ''
     },
-    price: {
+    price : {
         type: Number,
-        default: 0,
+        default:0
     },
     category: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category',
+        required:true
     },
-
     countInStock: {
         type: Number,
         required: true,
         min: 0,
-        max: 255,
+        max: 255
     },
     rating: {
         type: Number,
@@ -55,7 +55,6 @@ const productSchema = mongoose.Schema({
         type: Date,
         default: Date.now,
     },
-
 })
 
 exports.Product = mongoose.model('Product', productSchema);
