f9e90648fc0f3aa6fd50fdfbcb096c1fa31313c3
AlainDlcTVOP <alain_dlc@outlook.com>
2021-07-28 20:02:19 +0200

    get products without aut (frontend as customer)

--- backend/helper/jwt.js
+++ backend/helper/jwt.js
@@ -3,10 +3,19 @@ const expressJwt = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.secret;
+    const api = process.env.API_URL;
     return expressJwt({
         secret,
         algorithms: ['HS256']
 
+    }).unless({
+        path: [
+            // regex online
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },// exlude aut after products
+             {url: /\/api\/v1\/categories(.*)/ ,methods: ['GET','OPTIONS']}, // exlude aut after categories
+            `${api}/users/login`,
+            `${api}/users/register`,
+        ]
     })
 }
 
