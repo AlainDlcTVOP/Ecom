7752c863dc7509af4fdeb4f4cfddb0d5ba9f0690
AlainDlcTVOP <alain_dlc@outlook.com>
2021-08-02 20:41:11 +0200

    user API done

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -64,6 +64,29 @@ router.post('/register', async (req,res)=>{
     res.send(user);
 })
 
+router.delete('/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id).then(user =>{
+        if (user) {
+            return res.status(200).json({success:true, message: 'the user is deleted!'})
+        } else {
+              return res.status(404).json({success:false, message: 'the user not found!'})
+        }
+    }).catch(err => {
+        return res.status(404).json({success:false, error: err})
+    })
+})
+
+router.get(`/get/count`, async (req, res) =>{
+    const userCount = await User.countDocuments((count) => count)
+
+    if(!userCount) {
+        res.status(500).json({success: false})
+    } 
+    res.send({
+        userCount:userCount
+    });
+})
+
 
 router.post('/login', async (req,res) => {
     const user = await User.findOne({email: req.body.email})
