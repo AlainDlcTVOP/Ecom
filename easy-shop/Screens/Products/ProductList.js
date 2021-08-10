4462e3baa1787b1f63ce35db36256b08be7ed5f6
AlainDlcTVOP <alain_dlc@outlook.com>
2021-08-10 21:44:50 +0200

    refactor and fix productCard

--- easy-shop/Screens/Products/ProductList.js
+++ easy-shop/Screens/Products/ProductList.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { View, TouchableOpacity, Dimensions } from "react-native";
+import ProductCard from "./ProductCard";
 
-let { width } = Dimensions.get("window");
+var { width } = Dimensions.get("window");
 // set defualt window screen
 const ProductList = (props) => {
+  const { item } = props;
   return (
     <TouchableOpacity style={{ width: "50%" }}>
-      <View style={{ width: width / 2, backgroundColor: "gainsboro" }}></View>
+      <View
+        style={{
+          width: width / 2,
+          backgroundColor: "gainsboro",
+        }}
+      >
+        <ProductCard {...item} />
+      </View>
     </TouchableOpacity>
   );
 };
