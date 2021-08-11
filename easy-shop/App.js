7bfd2c90bac656a724914ed9305e78362643190f
AlainDlcTVOP <alain_dlc@outlook.com>
2021-08-11 21:48:29 +0200

    add new image in assets and creat new component header

--- easy-shop/App.js
+++ easy-shop/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View , LogBox} from "react-native";
 import ProductContainer from "./Screens/Products/ProductContainer";
+import Header from "./Shared/Header";
 
 export default function App() {
   LogBox.ignoreLogs(['Remote debugger']);
   return (
     <View style={styles.container}>
+     <Header />
       <ProductContainer />
     </View>
   );
