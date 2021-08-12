b4d928d05013515e4f005a3a7a8c61bd45ba7cdf
AlainDlcTVOP <alain_dlc@outlook.com>
2021-08-12 23:15:55 +0200

    searchlist updated bug fix 2021-08-12

--- easy-shop/Screens/Products/ProductContainer.js
+++ easy-shop/Screens/Products/ProductContainer.js
@@ -4,49 +4,109 @@ import {
   StyleSheet,
   ActivityIndicator,
   FlatList,
+  ScrollView,
+  Dimensions
 } from "react-native";
 import { Container, Header, Icon, Item, Input, Text } from 'native-base';
-
+import SearchedProduct from './SearchedProducts';
 import ProductList from "./ProductList";
-const data = require("../../assets/data/products.json");
+const data = require('../../assets/data/products.json');
 
-const ProductContainer = () => {
-  const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    setProducts(data);
-
-    return () => {
-      setProducts([]);
+var { height } = Dimensions.get('window')
+ const ProductContainer = () =>{
+ 
+    const [products, setProducts] = useState([]);
+    const [productsFiltered, setProductsFiltered] = useState([]);
+    const [focus, setFocus] = useState();
+ 
+    useEffect(() =>{
+        setProducts(data);
+        setProductsFiltered(data);
+        setFocus(false);
+ 
+        return () =>{
+            setProducts([])
+            setProductsFiltered([])
+            setFocus();
+        }
+    }, [])
+ 
+    const searchProduct = (text) =>{
+        setProductsFiltered(
+            products.filter((i) => i.name.toLowerCase().includes(text.toLowerCase()))
+        );
     };
-  }, []);
-
-  return (
-    <Container>
-      <Header searchBar rounded >
-        <Item>
-          <Icon name='ios-search' />
-          <Input
-            placeholder='Search'
-            //onFocus={}
-            // onChangeText={{text} =>}
-          />
-        </Item>
-      </Header>
-    <View>
-      <Text>Product Container</Text>
-      <View style={{ marginTop: 100 }}>
-        <FlatList
-         horizontal={false}
-         numColumns={2}
-          data={products}
-          renderItem={({ item }) => <ProductList key={item.id} item={item} />}
-          keyExtractor={(item) => item.name}
-        />
-      </View>
-      </View>
-    </Container>
-  );
-};
-
+ 
+    const openList = () =>{
+        setFocus(true);
+    } 
+ 
+    const onBlur = () =>{
+        setFocus(false);
+    }
+ 
+    return(
+        <Container>
+            <Header searchBar rounded>
+                <Item>
+                    <Icon name='ios-search' />
+                    <Input 
+                    placeholder="Search"
+                    onFocus={openList}
+                    onChangeText={(text) => searchProduct(text)}
+                    />
+                    {focus==true ? (
+                        <Icon onPress={onBlur} name="ios-close" />
+                    ):null}
+                </Item>
+            </Header>
+            {focus==true ? (
+                <SearchedProduct
+                productsFiltered={productsFiltered}
+                />
+            ):(
+            <View style={styles.container}>
+            <View >
+                
+            </View>
+            <View style={{marginTop:100}}>
+            <FlatList
+               
+                data={products}
+                renderItem={({item}) => <ProductList 
+                key={item.id} 
+                item={item}
+                /> }
+                numColumns={2}
+                keyExtractor={item => item.name}
+            />
+            </View>
+        </View>
+            )}
+            
+        </Container>
+        
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+      flexWrap: "wrap",
+      backgroundColor: "gainsboro",
+    },
+    listContainer: {
+      height: height,
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "flex-start",
+      flexWrap: "wrap",
+      backgroundColor: "gainsboro",
+    },
+    center: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+  });
+ 
+ 
 export default ProductContainer;
