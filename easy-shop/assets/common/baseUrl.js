6e161da041d26d952b277b6cd57c16b66ed6a386
AlainDlcTVOP <alain_dlc@outlook.com>
2021-10-11 14:25:54 +0200

    order

--- easy-shop/assets/common/baseUrl.js
+++ easy-shop/assets/common/baseUrl.js
@@ -5,7 +5,7 @@ import { Platform } from 'react-native'
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://5d79-213-102-85-120.ngrok.io/api/v1/'
+? baseURL = 'http://8b4b-213-102-85-120.ngrok.io/api/v1/'
 : baseURL = 'http://localhost:3000/api/v1/'
 }
 
