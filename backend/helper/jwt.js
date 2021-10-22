361427535f98b92663649cbddcfa7ef4874c9278
AlainDlcTVOP <alain_dlc@outlook.com>
2021-10-22 11:03:59 +0200

    jwt update

--- backend/helper/jwt.js
+++ backend/helper/jwt.js
@@ -1,38 +1,32 @@
-// express-jwt for protecting the Token
 const expressJwt = require('express-jwt');
 
 function authJwt() {
     const secret = process.env.secret;
     const api = process.env.API_URL;
-
-    return expressJwt({secret, algorithms: ['HS256'], isRevoked}).unless({
+    return expressJwt({
+        secret,
+        algorithms: ['HS256'],
+        isRevoked: isRevoked
+    }).unless({
         path: [
-            // public routes that don't require authentication
-            /\/public\/uploads(.*)/,
+            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
             `${api}/users/login`,
             `${api}/users/register`,
-            {
-                url: /\/api\/v1\/products(.*)/,
-                methods: ['GET', 'OPTIONS']
-            }, {
-                url: /\/api\/v1\/users(.*)/,
-                methods: ['GET', 'OPTIONS']
-            }, {
-                url: /\/api\/v1\/orders(.*)/,
-                methods: ['GET', 'OPTIONS', 'POST']
-            }, {
-                url: /\/api\/v1\/categories(.*)/,
-                methods: ['GET', 'OPTIONS']
-            },
         ]
-    });
+    })
 }
 
 async function isRevoked(req, payload, done) {
-    if (! payload.isAdmin) {
-        done(null, true) // reject the token if not admin
-    }done();
+    if(!payload.isAdmin) {
+        done(null, true)
+    }
 
+    done();
 }
 
-module.exports = authJwt;
+
+
+module.exports = authJwt
