a1737841c709865e29adb4681a773943024f6fa4
AlainDlcTVOP <alain_dlc@outlook.com>
2021-10-22 10:57:53 +0200

    make it better

--- backend/helper/jwt.js
+++ backend/helper/jwt.js
@@ -5,16 +5,27 @@ function authJwt() {
     const secret = process.env.secret;
     const api = process.env.API_URL;
 
-    return expressJwt({ secret, algorithms: ['HS256'], isRevoked })
-.unless({ path: [ 
-// public routes that don't require authentication
- /\/public\/uploads(.*)/,
- `${api}/users/login`,
- `${api}/users/register`,
- { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
- { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'OPTIONS'] },
- { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'], }, 
-{ url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] }, ], });
+    return expressJwt({secret, algorithms: ['HS256'], isRevoked}).unless({
+        path: [
+            // public routes that don't require authentication
+            /\/public\/uploads(.*)/,
+            `${api}/users/login`,
+            `${api}/users/register`,
+            {
+                url: /\/api\/v1\/products(.*)/,
+                methods: ['GET', 'OPTIONS']
+            }, {
+                url: /\/api\/v1\/users(.*)/,
+                methods: ['GET', 'OPTIONS']
+            }, {
+                url: /\/api\/v1\/orders(.*)/,
+                methods: ['GET', 'OPTIONS', 'POST']
+            }, {
+                url: /\/api\/v1\/categories(.*)/,
+                methods: ['GET', 'OPTIONS']
+            },
+        ]
+    });
 }
 
 async function isRevoked(req, payload, done) {
